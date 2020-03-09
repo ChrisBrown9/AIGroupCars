4775b0f4ee7133492fbd1727bd80c72caca3bc1a
Azirsec <43415156+Azirsec@users.noreply.github.com>
2020-03-08 21:15:17 -0400

    Added the car
    Car and 2 or 3 scripts, one has the car sensors the other has the car actions

---
+++ AI Group Cars/Assets/Scripts/AICarSensors.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICarSensors : MonoBehaviour
+{
+    RaycastHit Forward;
+    RaycastHit FrontLeft;
+    RaycastHit FrontRight;
+    RaycastHit FrontDown;
+    RaycastHit Left;
+    RaycastHit Right;
+    RaycastHit BackLeft;
+    RaycastHit BackRight;
+
+    // Makes all the lines that come out from the car, in order to let the car see where its going
+    void Update()
+    {
+        
+        Physics.Raycast(transform.position + transform.forward * 1.3f, transform.forward, out Forward, 50);
+        //Debug.DrawLine(transform.position + transform.forward * 1.3f, Forward.point);
+
+        Physics.Raycast(transform.position + transform.forward * 1.3f, (transform.forward * 2 - transform.right).normalized, out FrontLeft, 50);
+        //Debug.DrawLine(transform.position + transform.forward * 1.3f, FrontLeft.point);
+
+        Physics.Raycast(transform.position + transform.forward * 1.3f, (transform.forward * 2 + transform.right).normalized, out FrontRight, 50);
+        //Debug.DrawLine(transform.position + transform.forward * 1.3f, FrontRight.point);
+
+        Physics.Raycast(transform.position + transform.forward * 1.3f, (transform.forward * 2 - transform.up).normalized, out FrontDown, 50);
+        //Debug.DrawLine(transform.position + transform.forward * 1.3f, FrontDown.point);
+
+        Physics.Raycast(transform.position - transform.forward * 1.3f, (-transform.forward * 2 - transform.right).normalized, out BackLeft, 50);
+        //Debug.DrawLine(transform.position - transform.forward * 1.3f, BackLeft.point);
+
+        Physics.Raycast(transform.position - transform.forward * 1.3f, (-transform.forward * 2 + transform.right).normalized, out BackRight, 50);
+        //Debug.DrawLine(transform.position - transform.forward * 1.3f, BackRight.point);
+    }
+
+
+    public float ForwardDist()
+    {
+        return Forward.distance;
+    }
+
+    public float FrontLeftDist()
+    {
+        return FrontLeft.distance;
+    }
+
+    public float FrontRightDist()
+    {
+        return FrontRight.distance;
+    }
+
+    public float FrontDownDist()
+    {
+        return FrontDown.distance;
+    }
+
+    public float BackLeftDist()
+    {
+        return BackLeft.distance;
+    }
+
+    public float BackRightDist()
+    {
+        return BackRight.distance;
+    }
+
+}
---
+++ AI Group Cars/Assets/Scripts/AIDrivingActions.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDrivingActions : MonoBehaviour
+{
+    bool grounded = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x * 0.95f, 
+            GetComponent<Rigidbody>().velocity.y, 
+            GetComponent<Rigidbody>().velocity.z *0.95f);
+    }
+
+    public void TurnLeft()
+    {
+        if (grounded)
+        {
+            transform.Rotate(new Vector3(0, -2, 0));
+        }
+    }
+
+    public void TurnRight()
+    {
+        if (grounded)
+        {
+            transform.Rotate(new Vector3(0, 2, 0));
+        }
+    }
+
+    public void Accelerate()
+    {
+        if (grounded)
+        {
+            GetComponent<Rigidbody>().AddForce(transform.forward * 0.5f, ForceMode.Impulse);
+        }
+    }
+
+    public void Decelerate()
+    {
+        if (grounded)
+        {
+            GetComponent<Rigidbody>().AddForce(-transform.forward * 0.5f, ForceMode.Impulse);
+        }
+    }
+
+    public void Jump()
+    {
+        if (grounded)
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, 0), ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Track")
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Track")
+        {
+            grounded = false;
+        }
+    }
+}
